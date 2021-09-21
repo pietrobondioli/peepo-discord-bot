2b04b16eb915984714de5cb5464dd93ba01d2f7a
Pietro Bondioli <pietrobondiolideveloper@gmail.com>
2021-09-21 20:43:54 -0300

    feat: add 'private' functionality for roll commands
    Roll commands now accepts an optional argument '-p [me, @user,]' that
    changes the default behavior of the strategy when sending the response
    message (it's only send for the users quoted in the command).

--- src/commands/RollCommand/RollCommandsList.ts
+++ src/commands/RollCommand/RollCommandsList.ts
@@ -4,15 +4,15 @@ import { ICommandList } from '../base/CommandContextBase';
 
 const RollCommandsList: ICommandList = {
   [RollCommandType.RollOneDice]: new RegExp(
-    `^(?:${Constants.DEFAULT_BOT_COMMANDS_PREFIX}){1}(?:roll){1}(?: +| +d?| +1? +d)(?<diceNumber>[2-9]{1}\\d{0,2}|1\\d{1,3})(?<additionalOperations>(?: ?[+\\-*/]\\d{1,2})+)?$`,
+    `^(?:${Constants.DEFAULT_BOT_COMMANDS_PREFIX}){1}(?:roll){1}(?: +1? ?d)(?<diceNumber>[2-9]{1}\\d{0,2}|1\\d{1,3})(?<additionalOperations>(?: ?[+\\-*/]\\d{1,2})+)? ?(?:(?<isPrivate>-p) ?(?<selfSendPrivateMessage>me)? ?(?<sendResponseTo>(?:<@!\\d+> ?){1,3}))? *$`,
     Constants.DEFAULT_COMMAND_REGEX_FLAGS
   ),
   [RollCommandType.RollMultipleDices]: new RegExp(
-    `^(?:${Constants.DEFAULT_BOT_COMMANDS_PREFIX}){1}(?:roll){1} +(?<numberOfDices>[2-9]{1}\\d{0,1}|1\\d{1}) d(?<diceNumber>[2-9]{1}\\d{0,2}|1\\d{1,3})(?<additionalOperations>(?: ?[+\\-*/]\\d{1,2})+)?$`,
+    `^(?:${Constants.DEFAULT_BOT_COMMANDS_PREFIX}){1}(?:roll){1} +(?<numberOfDices>[2-9]{1}\\d{0,1}|1\\d{1}) d(?<diceNumber>[2-9]{1}\\d{0,2}|1\\d{1,3})(?<additionalOperations>(?: ?[+\\-*/]\\d{1,2})+)? ?(?:(?<isPrivate>-p) ?(?<selfSendPrivateMessage>me)? ?(?<sendResponseTo>(?:<@!\\d+> ?){1,3}))? *$`,
     Constants.DEFAULT_COMMAND_REGEX_FLAGS
   ),
   [RollCommandType.RollMultipleDicesAndSum]: new RegExp(
-    `^(?:${Constants.DEFAULT_BOT_COMMANDS_PREFIX}){1}(?:roll){1} +(?<numberOfDices>[2-9]{1}\\d{0,1}|1\\d{1})d(?<diceNumber>[2-9]{1}\\d{0,2}|1\\d{1,3})(?<additionalOperations>(?: ?[+\\-*/]\\d{1,2})+)?$`,
+    `^(?:${Constants.DEFAULT_BOT_COMMANDS_PREFIX}){1}(?:roll){1} +(?<numberOfDices>[2-9]{1}\\d{0,1}|1\\d{1})d(?<diceNumber>[2-9]{1}\\d{0,2}|1\\d{1,3})(?<additionalOperations>(?: ?[+\\-*/]\\d{1,2})+)? ?(?:(?<isPrivate>-p) ?(?<selfSendPrivateMessage>me)? ?(?<sendResponseTo>(?:<@!\\d+> ?){1,3}))? *$`,
     Constants.DEFAULT_COMMAND_REGEX_FLAGS
   ),
 };
