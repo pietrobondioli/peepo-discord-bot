17a776306443e28ff52aec24875ea92d19b5b55b
Pietro Bondioli <pietrobondiolideveloper@gmail.com>
2021-10-15 00:12:40 -0300

    feat: create OpenWeather service

---
+++ src/models/services/OpenWeather/WeatherResponse.ts
@@ -0,0 +1,45 @@
+type Weather = {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+};
+
+interface WeatherResponse {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: Weather[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  visibility: string;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+export { WeatherResponse };
